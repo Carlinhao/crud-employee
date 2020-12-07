5f1b7c7f86451b2ff0772ae46496e284ee0d592e
Carlinhao <carsilva.ti@gmail.com>
2020-12-07 00:01:31 -0300

    Implement employer repository

--- src/employers.infrastructure/Repositories/Employer/EmployerRepository.cs
+++ src/employers.infrastructure/Repositories/Employer/EmployerRepository.cs
@@ -1,21 +1,45 @@
-﻿using employers.domain.Entities.Employer;
+﻿using Dapper;
+using employers.domain.Entities.Employer;
 using employers.domain.Interfaces.Repositories.Employers;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace employers.infrastructure.Repositories.Employer
 {
     public class EmployerRepository : IEmployerRepository
     {
-        
-        public Task<IEnumerable<EmployerEntity>> GetAll()
+        private readonly IConfiguration _configuration;
+
+        public IDbConnection Connection
         {
-            throw new System.NotImplementedException();
+            get
+            {
+                return new SqlConnection(_configuration.GetConnectionString("sqlConnect"));
+            }
         }
 
-        public Task<EmployerEntity> GetById(object id)
+        public async Task<IEnumerable<EmployerEntity>> GetAll()
         {
-            throw new System.NotImplementedException();
+            using IDbConnection conn = Connection;
+            string query = "SELECT ID_DPTO, NOM_DPTO FROM Empregado";
+            conn.Open();
+            var result = await conn.QueryAsync<EmployerEntity>(query);
+
+            return result.ToList();
+        }
+
+        public async Task<EmployerEntity> GetById(object id)
+        {
+            using IDbConnection conn = Connection;
+            string query = $"SELECT ID_DPTO, NOM_DPTO FROM Empregado WHERE ID_EMP = { id }";
+            conn.Open();
+            var result = await conn.QueryAsync<EmployerEntity>(query);
+
+            return result.FirstOrDefault();
         }
     }
 }
