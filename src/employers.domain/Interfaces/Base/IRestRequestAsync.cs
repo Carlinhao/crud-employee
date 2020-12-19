e209b930547c46358e444a5b2b8521ecdd8b4fcb
Carlinhao <carsilva.ti@gmail.com>
2020-12-18 23:50:07 -0300

    Add json property name, removing interface.

--- src/employers.domain/Interfaces/Base/IRestRequestAsync.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace employers.domain.Interfaces.Base
-{
-    public interface IRestRequestAsync<TRequest, TResponse>
-    {
-        Task<TResponse> RunAsync(TRequest request);
-    }
-}
--- src/employers.domain/Responses/DepartamentResponse.cs
+++ src/employers.domain/Responses/DepartamentResponse.cs
@@ -1,10 +1,12 @@
 ﻿using employers.domain.Entities;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace employers.domain.Responses
 {
     public class DepartamentResponse
     {
+        [JsonPropertyName("departments")]
         public List<DepartmentEntity> Departaments { get; set; }
     }
 }
