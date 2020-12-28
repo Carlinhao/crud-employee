3caf5bffd594405f7a62bc52377ce2edf72b5bed
Carlinhao <carsilva.ti@gmail.com>
2020-12-27 22:55:04 -0300

    Create teste builder Employer.

---
+++ test/employer.application.tests/Utils/EmployerBuilder.cs
@@ -0,0 +1,38 @@
+﻿using employers.domain.Entities.Employer;
+
+namespace employer.application.tests.Utils
+{
+    public class EmployerBuilder
+    {
+        private int _id;
+        private string _name;
+        private int _idDepartament;
+
+        public static EmployerBuilder Novo()
+        {
+            return new EmployerBuilder();
+        }
+
+        public EmployerBuilder WhithId(int id)
+        {
+            _id = id;
+            return this;
+        }
+        public EmployerBuilder WhithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+        public EmployerBuilder WhithIdDepartment(int idDepartament)
+        {
+            _idDepartament = idDepartament;
+            return this;
+        }
+
+        public EmployerEntity Build()
+        {
+            var employer = new EmployerEntity { Id = _id, IdDepartament = _idDepartament, Name = _name };
+            return employer;
+        }
+    }
+}
