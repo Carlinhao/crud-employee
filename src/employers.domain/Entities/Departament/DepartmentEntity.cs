935cb0196c8e812b1e9d686c6279947451141610
Carlinhao <carsilva.ti@gmail.com>
2021-01-06 23:14:41 -0300

    Refactor code.

--- src/employers.domain/Entities/Departament/DepartmentEntity.cs
+++ src/employers.domain/Entities/Departament/DepartmentEntity.cs
@@ -1,8 +1,7 @@
 ﻿namespace employers.domain.Entities
 {
-    public class DepartmentEntity
+    public class DepartmentEntity : EntityBase
     {        
-        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
--- src/employers.domain/Entities/Employer/EmployerEntity.cs
+++ src/employers.domain/Entities/Employer/EmployerEntity.cs
@@ -1,8 +1,7 @@
 ﻿namespace employers.domain.Entities.Employer
 {
-    public class EmployerEntity
+    public class EmployerEntity : EntityBase
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public int IdDepartament { get; set; }
     }
