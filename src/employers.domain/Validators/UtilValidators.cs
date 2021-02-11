54cf76fb67779d904bced97a52ea199fbc90bd8d
Carlinhao <carsilva.ti@gmail.com>
2021-02-10 21:57:28 -0300

    Create generic class UtilValidator

---
+++ src/employers.domain/Validators/UtilValidators.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace employers.domain.Validators
+{
+    public static class UtilValidators
+    {
+        public static ValidationResult ValidadorResult<TV, TE>(TV validacao, TE entidade)
+            where TV : AbstractValidator<TE> where TE : class
+        {
+            return validacao.Validate(entidade);
+        }
+    }
+}
