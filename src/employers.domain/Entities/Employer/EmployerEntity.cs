2683bbaa87fc4a177f3ca8645b6b5eac98fbd737
Carlinhao <carsilva.ti@gmail.com>
2021-02-16 23:18:01 -0300

    Add fluent validation for validate Entity

--- src/employers.domain/Entities/Employer/EmployerEntity.cs
+++ src/employers.domain/Entities/Employer/EmployerEntity.cs
@@ -1,8 +1,34 @@
-﻿namespace employers.domain.Entities.Employer
+﻿using FluentValidation;
+
+namespace employers.domain.Entities.Employer
 {
     public class EmployerEntity : EntityBase
     {
         public string Name { get; set; }
         public int IdDepartament { get; set; }
+    }
+
+    public class EmployerUpdateValidator : AbstractValidator<EmployerEntity>
+    {
+        public EmployerUpdateValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Id não informado")
+                .NotNull()
+                .WithMessage("Id não informado");
+
+            RuleFor(x => x.IdDepartament)
+                .NotEmpty()
+                .WithMessage("Id do Departamento não informado")
+                .NotNull()
+                .WithMessage("Id do Departamento não informado");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Employer não informado")
+                .NotNull()
+                .WithMessage("Employer não informado");
+        }
     }
 }
