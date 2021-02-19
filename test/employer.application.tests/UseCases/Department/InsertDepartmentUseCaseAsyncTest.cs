47fc4a508bfa4c6f32739e992f3014b52a2dbb32
Carlinhao <carsilva.ti@gmail.com>
2021-02-18 21:44:50 -0300

    Insert new unit test for InsertDepartmentUseCaseAsync.

---
+++ test/employer.application.tests/UseCases/Department/InsertDepartmentUseCaseAsyncTest.cs
@@ -0,0 +1,71 @@
+﻿using employers.application.Notifications;
+using employers.application.UseCases.Departament;
+using employers.domain.Interfaces.Repositories.Departament;
+using employers.domain.Requests;
+using Moq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace employer.application.tests.UseCases.Department
+{
+    public class InsertDepartmentUseCaseAsyncTest
+    {
+        private readonly Mock<IDepartmentRepository> _departmentRepository;
+        private readonly Mock<INotificationMessages> _notificationMessages;
+
+        public InsertDepartmentUseCaseAsyncTest()
+        {
+            _departmentRepository = new Mock<IDepartmentRepository>();
+            _notificationMessages = new Mock<INotificationMessages>();
+        }
+
+        [Fact(DisplayName = "Insert Department Success")]
+        [Trait("Category", "Department")]
+        public async Task InsertDepartmentUseCaseAsync_WhenDataIsValid_MustInsertDepartment()
+        {
+            // Arrange
+            var useCase = GetInsertUseCase();
+            var request = GetDepartmentRequest();
+
+            // Act
+            _departmentRepository.Setup(x => x.InsertAsync(request)).ReturnsAsync(1);
+            var result = await useCase.RunAsync(request);
+
+            // Assert
+            Assert.Equal(1, result.Value);
+        }
+
+        [Theory(DisplayName = "Insert Department Success")]
+        [Trait("Category", "Department")]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task InsertDepartmentUseCaseAsync_WhenDataNotValid_MustReturnNotification(string departmentRequest)
+        {
+            // Arrange
+            var useCase = GetInsertUseCase();
+            var request = new DepartmentRequest() { Nome = departmentRequest };
+
+            // Act
+            var result = await useCase.RunAsync(request);
+
+            // Assert
+            _notificationMessages.Verify(x => x.AddNotification("InsertDepartmentUseCaseAsync", It.IsAny<string>(), HttpStatusCode.BadRequest), Times.Once);
+        }
+
+
+        private InsertDepartmentUseCaseAsync GetInsertUseCase()
+        {
+            return new InsertDepartmentUseCaseAsync(_departmentRepository.Object,
+                                                    _notificationMessages.Object);
+        }
+
+        private DepartmentRequest GetDepartmentRequest()
+        {
+            return new DepartmentRequest
+            {
+                Nome = "IT"
+            };
+        }
+    }
+}
