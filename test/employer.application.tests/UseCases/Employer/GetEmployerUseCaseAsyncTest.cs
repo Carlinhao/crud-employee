f37058013eba845b467edb6e39716ccc428ae487
Carlinhao <carsilva.ti@gmail.com>
2021-02-18 22:51:13 -0300

    Add unit test GetEmployerUseCaseAsync

---
+++ test/employer.application.tests/UseCases/Employer/GetEmployerUseCaseAsyncTest.cs
@@ -0,0 +1,56 @@
+﻿using employers.application.UseCases.Employers;
+using employers.domain.Entities.Employer;
+using employers.domain.Interfaces.Repositories.Employers;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace employer.application.tests.UseCases.Employer
+{
+    public class GetEmployerUseCaseAsyncTest
+    {
+        private readonly Mock<IEmployerRepository> _employerRepository;
+
+        public GetEmployerUseCaseAsyncTest()
+        {
+            _employerRepository = new Mock<IEmployerRepository>();
+        }
+
+        [Fact(DisplayName = "Must return all Employer")]
+        [Trait("Category", "Employer")]
+        public async Task GetEmployerUseCaseAsync_WhenRequestSuccess_MustReturnAllEmployer()
+        {
+            // Arrange
+            var useCase = new GetEmployerUseCaseAsync(_employerRepository.Object);
+            var employer = GetEmployer();
+
+            // Act
+            _employerRepository.Setup(x => x.GetAll()).ReturnsAsync(employer);
+            var result = await useCase.RunAsync();
+
+            // Assert
+            Assert.True(result.ToList().Count() == 4);
+
+            Assert.Collection(result, 
+                item => Assert.Equal("IT", item.Name),
+                item => Assert.Equal("HR", item.Name),
+                item => Assert.Equal("Business", item.Name),
+                item => Assert.Equal("Manager", item.Name));
+        }
+
+        private IEnumerable<EmployerEntity> GetEmployer()
+        {
+            var result = new List<EmployerEntity>()
+            {
+                new EmployerEntity { Id = 1, IdDepartament = 2, Name = "IT" },
+                new EmployerEntity { Id = 2, IdDepartament = 4, Name = "HR" },
+                new EmployerEntity { Id = 3, IdDepartament = 6, Name = "Business" },
+                new EmployerEntity { Id = 4, IdDepartament = 9, Name = "Manager" },
+            };
+
+            return result;
+        }
+    }
+}
