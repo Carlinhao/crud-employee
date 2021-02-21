aeddedcb0cea89cc319346a25544d3b06eeed0a6
Carlinhao <carsilva.ti@gmail.com>
2021-02-21 18:46:33 -0300

    Create base test for UpdateEmployerUseCaseAsync, create fake methods.

---
+++ test/employer.application.tests/UseCases/Employer/UpdateEmployerUseCaseAsyncTest.cs
@@ -0,0 +1,59 @@
+﻿using employers.application.Notifications;
+using employers.application.UseCases.Employers;
+using employers.domain.Entities.Employer;
+using employers.domain.Interfaces.Repositories.Employers;
+using employers.domain.Responses;
+using Moq;
+using Xunit;
+
+namespace employer.application.tests.UseCases.Employer
+{
+    public class UpdateEmployerUseCaseAsyncTest
+    {
+        private readonly Mock<IEmployerRepository> _employerRepository;
+        private readonly Mock<INotificationMessages> _notificationMessages;
+
+        public UpdateEmployerUseCaseAsyncTest()
+        {
+            _employerRepository = new Mock<IEmployerRepository>();
+            _notificationMessages = new Mock<INotificationMessages>();
+        }
+
+        [Fact(DisplayName = "Must update Employer")]
+        [Trait("Category", "Employer")]
+        public void Trocar_Nome_Metodo()
+        {
+            // Arrange
+            var useCase = UpdateEmployerUseCase();
+            var request = GetEmployerEntity();
+            var response = GetResponse();
+
+            // Act
+            _employerRepository.Setup(x => x.UpdateAsync(request)).ReturnsAsync(response);
+
+            // Assert
+        }
+
+        private UpdateEmployerUseCaseAsync UpdateEmployerUseCase()
+        {
+            return new UpdateEmployerUseCaseAsync(_employerRepository.Object, _notificationMessages.Object);
+        }
+
+        private EmployerEntity GetEmployerEntity()
+        {
+            return new EmployerEntity
+            {
+                Id = 1,
+                IdDepartament = 7,
+                Name = "Business"
+            };
+        }
+
+        private ResultResponse GetResponse()
+        {
+            var data = GetEmployerEntity();
+
+            return new ResultResponse { Data = data, Message = "Update employer success", Success = true };
+        }
+    }
+}
