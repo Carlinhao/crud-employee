e4a0d1b8974edd4318b2b387e7f4bcdac1969dcb
Carlinhao <carsilva.ti@gmail.com>
2021-02-20 21:12:33 -0300

    Add notfication for capture error in EmployerRequest

--- src/employers.application/UseCases/Employers/InsertEmployerUseCaseAsync.cs
+++ src/employers.application/UseCases/Employers/InsertEmployerUseCaseAsync.cs
@@ -1,6 +1,10 @@
 ﻿using employers.application.Interfaces.Empregado;
+using employers.application.Notifications;
 using employers.domain.Interfaces.Repositories.Employers;
 using employers.domain.Requests;
+using employers.domain.Validators;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace employers.application.UseCases.Employers
@@ -8,14 +12,27 @@ namespace employers.application.UseCases.Employers
     public class InsertEmployerUseCaseAsync : IInsertEmployerUseCaseAsync
     {
         private readonly IEmployerRepository _employerRepository;
+        private readonly INotificationMessages _notificationMessages;
 
-        public InsertEmployerUseCaseAsync(IEmployerRepository employerRepository)
+        public InsertEmployerUseCaseAsync(IEmployerRepository employerRepository,
+                                          INotificationMessages notificationMessages)
         {
             _employerRepository = employerRepository;
+            _notificationMessages = notificationMessages;
         }
 
         public async Task<int?> RunAsync(EmployerRequest request)
         {
+            var notification = UtilValidators.ValidadorResult(new EmployerRequestValidator(), request);
+
+            if (notification.Errors.Count() > 0)
+            {
+                foreach (var item in notification.Errors.Select(x => x.ErrorMessage).ToArray().Distinct())
+                {
+                    _notificationMessages.AddNotification("InsertEmployerUseCaseAsync", item, HttpStatusCode.BadRequest);
+                }
+            }
+
             var result = await _employerRepository.InsertAsync(request);
 
             return result;
