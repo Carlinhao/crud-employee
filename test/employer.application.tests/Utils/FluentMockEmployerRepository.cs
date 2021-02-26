c1ac80fa320e20fcb0f469bda2a61275f56631a9
Carlinhao <carsilva.ti@gmail.com>
2021-02-25 23:22:30 -0300

    Create first method  to return faker employer.

--- test/employer.application.tests/Utils/FluentMockEmployerRepository.cs
+++ test/employer.application.tests/Utils/FluentMockEmployerRepository.cs
@@ -1,10 +1,29 @@
-﻿using employers.domain.Interfaces.Repositories.Employers;
+﻿using employers.domain.Entities.Employer;
+using employers.domain.Interfaces.Repositories.Employers;
 using Moq;
+using System.Collections.Generic;
 
 namespace employer.application.tests.Utils
 {
     public class FluentMockEmployerRepository : Mock<IEmployerRepository>
     {
+        public FluentMockEmployerRepository GetAll()
+        {
+            var response = GetEmployers();
 
+            Setup(x => x.GetAll()).ReturnsAsync(response);
+            return this;
+        }
+
+        public IEnumerable<EmployerEntity> GetEmployers()
+        {
+            return new List<EmployerEntity>
+            {
+                new EmployerEntity { Id = 1, IdDepartament = 5, Name = "Paul Stone"},
+                new EmployerEntity { Id = 2, IdDepartament = 8, Name = "Maria Rita"},
+                new EmployerEntity { Id = 3, IdDepartament = 3, Name = "Lunna Iris"},
+                new EmployerEntity { Id = 4, IdDepartament = 9, Name = "Lis Bela"},
+            };
+        }
     }
 }
