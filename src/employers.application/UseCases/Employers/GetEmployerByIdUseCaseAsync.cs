e01b4577e32d308f1ddcdd1959f330fa0966bde0
Carlinhao <carsilva.ti@gmail.com>
2021-03-02 22:35:45 -0300

    Refactor code, add notification context to capture error message.

--- src/employers.application/UseCases/Employers/GetEmployerByIdUseCaseAsync.cs
+++ src/employers.application/UseCases/Employers/GetEmployerByIdUseCaseAsync.cs
@@ -1,7 +1,9 @@
 ﻿using employers.application.Exceptions.RegraNegocio;
 using employers.application.Interfaces.Empregado;
+using employers.application.Notifications;
 using employers.domain.Entities.Employer;
 using employers.domain.Interfaces.Repositories.Employers;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace employers.application.UseCases.Employers
@@ -9,16 +11,23 @@ namespace employers.application.UseCases.Employers
     public class GetEmployerByIdUseCaseAsync : IGetEmployerByIdUseCaseAsync
     {
         private readonly IEmployerRepository _employerRepository;
+        private readonly INotificationMessages _notification;
 
-        public GetEmployerByIdUseCaseAsync(IEmployerRepository employerRepository)
+        public GetEmployerByIdUseCaseAsync(IEmployerRepository employerRepository,
+                                           INotificationMessages notification)
         {
             _employerRepository = employerRepository;
+            _notification = notification;
         }
 
         public async Task<EmployerEntity> RunAsync(int id)
         {
             if (id <= 0)
-                throw new RegranegocioException("Invalid ID!");
+            {
+                _notification.AddNotification("GetEmployerByIdUseCaseAsync", "Invalid ID!", HttpStatusCode.BadRequest);
+
+                return new EmployerEntity();
+            }
 
             var result = await _employerRepository.GetById(id);
 
