22861b2ab18f510b757a988645866c57e9858da1
Carlinhao <carsilva.ti@gmail.com>
2021-03-02 22:40:45 -0300

    Refactor unit test to capture notification context message.

--- test/employer.application.tests/UseCases/Employer/GetEmployerByIdUseCaseAsyncTest.cs
+++ test/employer.application.tests/UseCases/Employer/GetEmployerByIdUseCaseAsyncTest.cs
@@ -1,7 +1,8 @@
-﻿using employers.application.Exceptions.RegraNegocio;
+﻿using employers.application.Notifications;
 using employers.application.UseCases.Employers;
 using employers.domain.Interfaces.Repositories.Employers;
 using Moq;
+using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,22 +11,26 @@ namespace employer.application.tests.UseCases.Employer
     public class GetEmployerByIdUseCaseAsyncTest
     {
         private readonly Mock<IEmployerRepository> _employerRepository;
+        private readonly Mock<INotificationMessages> _notification;
 
         public GetEmployerByIdUseCaseAsyncTest()
         {
             _employerRepository = new Mock<IEmployerRepository>();
+            _notification = new Mock<INotificationMessages>();
         }
 
         [Fact(DisplayName = "Testing invalid Id")]
         [Trait("Category", "Employer")]
         public async Task GetEmployerByIdUseCaseAsync_WhenInvalidID_MustDisplayMessage()
         {
-            // Arrange // Act
-            var useCase = new GetEmployerByIdUseCaseAsync(_employerRepository.Object);
+            // Arrange 
+            var useCase = new GetEmployerByIdUseCaseAsync(_employerRepository.Object, _notification.Object);
+
+            // Act
+            await useCase.RunAsync(0);
 
             // Assert
-            var ex = await Assert.ThrowsAsync<RegranegocioException>(() => useCase.RunAsync(0));
-            Assert.Equal("Invalid ID!", ex.Message);
+            _notification.Verify(x => x.AddNotification("GetEmployerByIdUseCaseAsync", "Invalid ID!", HttpStatusCode.BadRequest), Times.Once);
         }
     }
 }
