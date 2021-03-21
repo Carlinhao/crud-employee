aa70218040e1133a6de0ab3ce3831be479667a2d
Carlinhao <carsilva.ti@gmail.com>
2021-03-21 20:34:52 -0300

    Refactor unit test GetDepartamentUseCaseAsyncTest.

--- test/employer.application.tests/UseCases/Department/GetDepartamentUseCaseAsyncTest.cs
+++ test/employer.application.tests/UseCases/Department/GetDepartamentUseCaseAsyncTest.cs
@@ -21,24 +21,34 @@ namespace employer.application.tests.UseCases.Department
         [Trait("Category", "Department")]
         public async Task GetDepartamentUseCaseAsync_WhenCall_MustReturnAllDepartment()
         {
-            IEnumerable<DepartmentEntity> department = new List<DepartmentEntity> 
-            { 
+            // Arrange
+            var department = GetDepartment();
+            var useCase = new GetDepartamentUseCaseAsync(_departmentRepository.Object);
+            _departmentRepository.Setup(r => r.GetAll()).ReturnsAsync(department);
+
+            // Act
+            var result = await useCase.RunAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Collection(result, 
+                item => Assert.Equal(1, item.Id),
+                item => Assert.Equal(2, item.Id),
+                item => Assert.Equal(3, item.Id),
+                item => Assert.Equal(4, item.Id),
+                item => Assert.Equal(7, item.Id));
+        }
+
+        private IEnumerable<DepartmentEntity> GetDepartment()
+        {
+            return new List<DepartmentEntity>
+            {
                 new DepartmentEntity { Id = 1, Name = "TI"},
                 new DepartmentEntity { Id = 2, Name = "Sales"},
                 new DepartmentEntity { Id = 3, Name = "Business"},
                 new DepartmentEntity { Id = 4, Name = "Medical"},
                 new DepartmentEntity { Id = 7, Name = "Marketing"},
-            }; 
-
-
-            // Arrange
-            var useCase = new GetDepartamentUseCaseAsync(_departmentRepository.Object);
-
-            // Act
-            var teste = await useCase.RunAsync();
-
-            // Assert
-            Assert.NotNull(teste);
+            };
         }
     }
 }
