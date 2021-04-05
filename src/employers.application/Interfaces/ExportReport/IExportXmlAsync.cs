a7bc54ec8b5da3d81c6b7af2477d5a713e69b260
Carlinhao <carsilva.ti@gmail.com>
2021-04-04 21:02:11 -0300

    Create interface to export xml file

---
+++ src/employers.application/Interfaces/ExportReport/IExportXmlAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace employers.application.Interfaces.ExportReport
+{
+    public interface IExportXmlAsync
+    {
+        Task<string> ExportCsv(IEnumerable<object> request);
+    }
+}
