a0cdb3aa35b01e9d1bd121237202544892bfa92b
Carlinhao <carsilva.ti@gmail.com>
2021-04-04 21:03:51 -0300

    create interface tha export pdf file

---
+++ src/employers.application/Interfaces/ExportReport/IExportPdfAsync.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employers.application.Interfaces.ExportReport
+{
+    public interface IExportPdfAsync
+    {
+        Task<string> ExportPdf(IEnumerable<object> request);
+    }
+}
