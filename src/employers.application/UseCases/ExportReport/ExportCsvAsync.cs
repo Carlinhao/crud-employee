edb88fd75f085620b0f1d2336fa572dee2c1645b
Carlinhao <carsilva.ti@gmail.com>
2021-04-05 22:18:58 -0300

    Create classe tha export csv file.

---
+++ src/employers.application/UseCases/ExportReport/ExportCsvAsync.cs
@@ -0,0 +1,25 @@
+﻿using employers.application.Interfaces.ExportReport;
+using employers.domain.Entities.Employer;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employers.application.UseCases.ExportReport
+{
+    public class ExportCsvAsync : IExportCsvAsync
+    {
+        public async Task<string> ExportCsv(IEnumerable<EmployerEntity> request)
+        {
+            var sb = new StringBuilder();
+            foreach (var item in request)
+            {
+                sb.AppendFormat("{0},{1},{2},{3}",
+                     item.Id,
+                     item.IdDepartament,
+                     item.Name);
+                sb.AppendLine();
+            }
+            return await Task.FromResult(sb.ToString());
+        }
+    }
+}
