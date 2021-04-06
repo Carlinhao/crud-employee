db93f397fc2912c899ab9c93f79b4ca527c67829
Carlinhao <carsilva.ti@gmail.com>
2021-04-05 22:18:28 -0300

    Refactor type ienumerable.

--- src/employers.application/Interfaces/ExportReport/IExportCsvAsync.cs
+++ src/employers.application/Interfaces/ExportReport/IExportCsvAsync.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using employers.domain.Entities.Employer;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace employers.application.Interfaces.ExportReport
 {
     public interface IExportCsvAsync
     {
-        Task<string> ExportCsv(IEnumerable<object> request);
+        Task<string> ExportCsv(IEnumerable<EmployerEntity> request);
     }
 }
