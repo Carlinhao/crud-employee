a42b3edcca4e670993109af639c4af807f5539d3
Carlinhao <carsilva.ti@gmail.com>
2021-04-17 23:31:37 -0300

    Add  class fluentmocks to repository

---
+++ test/employer.repository.tests/FluentMockRepository/FluentMockDepartmentRepository.cs
@@ -0,0 +1,46 @@
+﻿using employers.domain.Entities;
+using employers.domain.Interfaces.Repositories.Departament;
+using employers.domain.Requests;
+using Moq;
+using System.Collections.Generic;
+
+namespace employer.repository.tests.FluentMockRepository
+{
+    public class FluentMockDepartmentRepository : Mock<IDepartmentRepository>
+    {
+        public FluentMockDepartmentRepository GetAll()
+        {
+            var response = GetAllDepartment();
+
+            Setup(x => x.GetAll()).ReturnsAsync(response);
+            return this;
+        }
+
+        public FluentMockDepartmentRepository GetById(object id)
+        {
+            var response = new DepartmentEntity { Id = 1, Name = "TI" };
+
+            Setup(x => x.GetById(id)).ReturnsAsync(response);
+            return this;
+        }
+
+        public FluentMockDepartmentRepository InsertAsync(DepartmentRequest request)
+        {
+            Setup(x => x.InsertAsync(request)).ReturnsAsync(1);
+
+            return this;
+        }
+
+        public IEnumerable<DepartmentEntity> GetAllDepartment()
+        {
+            return new List<DepartmentEntity>
+            {
+                new DepartmentEntity { Id = 1, Name = "TI"},
+                new DepartmentEntity { Id = 2, Name = "Sales"},
+                new DepartmentEntity { Id = 3, Name = "Business"},
+                new DepartmentEntity { Id = 4, Name = "Medical"},
+                new DepartmentEntity { Id = 7, Name = "Marketing"},
+            };
+        }
+    }
+}
---
+++ test/employer.repository.tests/FluentMockRepository/FluentMockEmployerRepository.cs
@@ -0,0 +1,71 @@
+﻿using employers.domain.Entities.Employer;
+using employers.domain.Interfaces.Repositories.Employers;
+using employers.domain.Requests;
+using employers.domain.Responses;
+using Moq;
+using System.Collections.Generic;
+
+namespace employer.repository.tests.FluentMockRepository
+{
+    public class FluentMockEmployerRepository : Mock<IEmployerRepository>
+    {
+        public FluentMockEmployerRepository GetById(object id)
+        {
+            var response = new EmployerEntity { Id = 1, IdDepartament = 5, Name = "Paul Stone" };
+
+            Setup(x => x.GetById(id)).ReturnsAsync(response);
+            return this;
+        }
+
+        public FluentMockEmployerRepository GetAll()
+        {
+            var response = GetEmployers();
+
+            Setup(x => x.GetAll()).ReturnsAsync(response);
+            return this;
+        }
+
+        public FluentMockEmployerRepository InsertAsync(EmployerRequest employerRequest)
+        {
+            Setup(x => x.InsertAsync(employerRequest)).ReturnsAsync(1);
+            return this;
+        }
+
+        public FluentMockEmployerRepository DeleteAsync(int id)
+        {
+            Setup(x => x.DeleteAsync(id)).ReturnsAsync(1);
+            return this;
+        }
+
+        public FluentMockEmployerRepository UpdateAsync(EmployerEntity entity)
+        {
+            var response = GetInsertResponse();
+
+            Setup(x => x.UpdateAsync(entity)).ReturnsAsync(response);
+            return this;
+        }
+
+        public ResultResponse GetInsertResponse()
+        {
+            var data = new EmployerEntity { Id = 1, IdDepartament = 5, Name = "Paul Stone" };
+
+            return new ResultResponse
+            {
+                Data = data,
+                Message = "",
+                Success = true
+            };
+        }
+
+        public IEnumerable<EmployerEntity> GetEmployers()
+        {
+            return new List<EmployerEntity>
+            {
+                new EmployerEntity { Id = 1, IdDepartament = 5, Name = "Paul Stone"},
+                new EmployerEntity { Id = 2, IdDepartament = 8, Name = "Maria Rita"},
+                new EmployerEntity { Id = 3, IdDepartament = 3, Name = "Lunna Iris"},
+                new EmployerEntity { Id = 4, IdDepartament = 9, Name = "Lis Bela"},
+            };
+        }
+    }
+}
