5be4bc552e2e5109423bfc83cfedd76c93753668
Carlinhao <carsilva.ti@gmail.com>
2021-04-21 18:16:29 -0300

    <type>(<scope>): <summary>
    <Describe the motivation behind this change - explain WHY you are making this change. Wrap all lines
    at 100 characters.>
    
    Fixes #<issue number>
    
    Add configuration to Api verssioning

--- src/employers.api/Startup.cs
+++ src/employers.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -60,13 +61,22 @@ namespace employers.api
            }));
 
             services.AddControllers();
-            services.AddApiVersioning( options => 
-            {
-                options.AssumeDefaultVersionWhenUnspecified = true;
-            });
 
             services.IocConfiguration();
             IOC.Rister();
+
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
             services.SwaggerServices();
             services.AddAutoMapper(typeof(Startup));
         }
