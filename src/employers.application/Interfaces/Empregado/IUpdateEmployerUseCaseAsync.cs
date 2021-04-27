a3c092c0da7a0fed5cdafa4c882b635ecc935d20
Carlinhao <carsilva.ti@gmail.com>
2021-04-26 23:33:33 -0300

    Refactor nome entitty in Interface

--- src/employers.application/Interfaces/Empregado/IUpdateEmployerUseCaseAsync.cs
+++ src/employers.application/Interfaces/Empregado/IUpdateEmployerUseCaseAsync.cs
@@ -4,7 +4,7 @@ using employers.domain.Responses;
 namespace employers.application.Interfaces.Empregado
 {
     public interface IUpdateEmployerUseCaseAsync : 
-        IRestRequestAsync<ResultResponse, EmployerEntity>
+        IRestRequestAsync<ResultResponse, EmployeeEntity>
     {        
     }
 }
