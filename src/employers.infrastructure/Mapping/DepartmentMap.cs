c90cac44c37885787548ff99b82f87487442637b
Carlinhao <carsilva.ti@gmail.com>
2021-04-26 23:45:21 -0300

    Add new colun in to database

---
+++ src/employers.infrastructure/Mapping/DepartmentMap.cs
@@ -0,0 +1,18 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using employers.domain.Entities;
+
+namespace employers.infrastructure.Mapping
+{
+    public class DepartmentMap : DommelEntityMap<DepartmentEntity>
+    {
+        public DepartmentMap()
+        {
+            ToTable("Departamento");
+
+            Map(x => x.Id).ToColumn("ID_DEPARTMENT").IsKey();
+            Map(x => x.Name).ToColumn("NOM_DEPARTMENT");
+            Map(x => x.Manager).ToColumn("MANAGER");
+            Map(x => x.Description).ToColumn("DESC_DEPARTMENT");
+        }
+    }
+}
