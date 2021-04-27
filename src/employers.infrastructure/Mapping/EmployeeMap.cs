f9c57e97ed46f2745ea0d42ef948f166414b2fe0
Carlinhao <carsilva.ti@gmail.com>
2021-04-26 23:45:58 -0300

    Add new colum in to database

---
+++ src/employers.infrastructure/Mapping/EmployeeMap.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using employers.domain.Entities.Employer;
+
+namespace employers.infrastructure.Mapping
+{
+    public class EmployeeMap : DommelEntityMap<EmployeeEntity>
+    {
+        public EmployeeMap()
+        {
+            ToTable("Employee");
+
+            Map(x => x.Id).ToColumn("ID_EMPLOYEE").IsKey();
+            Map(x => x.Name).ToColumn("NOM_EMPLOYEE");
+            Map(x => x.Active).ToColumn("ACTIVE");
+            Map(x => x.IdDepartament).ToColumn("ID_DEPARTMENT");
+            Map(x => x.IdOccupation).ToColumn("ID_OCCUPATION");
+        }
+    }
+}
