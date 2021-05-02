51d6bdde1b29b6e575fc076aba335fe9874c9c17
Carlinhao <carsilva.ti@gmail.com>
2021-05-02 17:55:13 -0300

    Add colunm to Mapping

--- src/employers.infrastructure/Mapping/EmployeeMap.cs
+++ src/employers.infrastructure/Mapping/EmployeeMap.cs
@@ -12,6 +12,7 @@ namespace employers.infrastructure.Mapping
             Map(x => x.Id).ToColumn("ID_EMPLOYEE").IsKey();
             Map(x => x.Name).ToColumn("NOM_EMPLOYEE");
             Map(x => x.Active).ToColumn("ACTIVE");
+            Map(x => x.Gender).ToColumn("GENDER");
             Map(x => x.IdDepartament).ToColumn("ID_DEPARTMENT");
             Map(x => x.IdOccupation).ToColumn("ID_OCCUPATION");
         }
