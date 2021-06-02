bc6e2f362935290d36738727e3811f74bc25c395
Carlinhao <carsilva.ti@gmail.com>
2021-06-01 23:59:10 -0300

    Adicioando os contratos da interface

--- src/employers.domain/Interfaces/Repositories/Occupation/IOccupationRepository.cs
+++ src/employers.domain/Interfaces/Repositories/Occupation/IOccupationRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace employers.domain.Interfaces.Repositories.Occupation
+﻿using employers.domain.Requests;
+using employers.domain.Responses;
+using System.Threading.Tasks;
+
+namespace employers.domain.Interfaces.Repositories.Occupation
 {
     public interface IOccupationRepository
     {
-
+        Task<ResultResponse> GetAllAsync();
+        Task<ResultResponse> UpdateAsync(OccupationRequest request);
     }
 }
