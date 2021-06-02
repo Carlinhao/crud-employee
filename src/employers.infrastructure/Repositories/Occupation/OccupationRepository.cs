28edca6ea113a725a4f85465d445230e12df0612
Carlinhao <carsilva.ti@gmail.com>
2021-06-01 23:58:20 -0300

    Adicionando os contratos do repository

--- src/employers.infrastructure/Repositories/Occupation/OccupationRepository.cs
+++ src/employers.infrastructure/Repositories/Occupation/OccupationRepository.cs
@@ -1,6 +1,34 @@
-﻿namespace employers.infrastructure.Repositories.Occupation
+﻿using employers.domain.Interfaces.Repositories.Occupation;
+using employers.domain.Requests;
+using employers.domain.Responses;
+using employers.infrastructure.DbConfiguration.Interfaces;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employers.infrastructure.Repositories.Occupation
 {
-    public class OccupationRepository
+    public class OccupationRepository : IOccupationRepository
     {
+        private readonly IDapperWrapper _conn;
+        private StringBuilder _stringBuilder;
+        private IDbConnection dbConnection;
+
+        public OccupationRepository(IDapperWrapper conn)
+        {
+            _conn = conn;
+            _stringBuilder = new StringBuilder();
+            dbConnection = _conn.GetConnection();
+        }
+
+        public Task<ResultResponse> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ResultResponse> UpdateAsync(OccupationRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
