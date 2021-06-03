1cc086e6ce4965c727aa333a6230ee44ac8f6bc5
Carlinhao <carsilva.ti@gmail.com>
2021-06-02 23:02:03 -0300

    Create use case and implementing method RunAsync

---
+++ src/employers.application/UseCases/Occupation/UpdateOccupationUseCaseAsync.cs
@@ -0,0 +1,25 @@
+﻿using employers.application.Interfaces.Occupation;
+using employers.domain.Interfaces.Repositories.Occupation;
+using employers.domain.Requests;
+using employers.domain.Responses;
+using System.Threading.Tasks;
+
+namespace employers.application.UseCases.Occupation
+{
+    public class UpdateOccupationUseCaseAsync : IUpdateOccupationUseCaseAsync
+    {
+        private readonly IOccupationRepository _occupationRepository;
+
+        public UpdateOccupationUseCaseAsync(IOccupationRepository occupationRepository)
+        {
+            _occupationRepository = occupationRepository;
+        }
+
+        public async Task<ResultResponse> RunAsync(OccupationUpdateRequest request)
+        {
+            var result = await _occupationRepository.UpdateAsync(request);
+
+            return result;
+        }
+    }
+}
