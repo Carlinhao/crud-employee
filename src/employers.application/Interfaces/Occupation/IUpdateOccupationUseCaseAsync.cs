9bc27977f5bb81c820420be6aadd1a38ca1ec03f
Carlinhao <carsilva.ti@gmail.com>
2021-06-02 23:01:34 -0300

    Create interface

---
+++ src/employers.application/Interfaces/Occupation/IUpdateOccupationUseCaseAsync.cs
@@ -0,0 +1,10 @@
+﻿using employers.domain.Requests;
+using employers.domain.Responses;
+
+namespace employers.application.Interfaces.Occupation
+{
+    public interface IUpdateOccupationUseCaseAsync :
+        IRestRequestAsync<ResultResponse, OccupationUpdateRequest>
+    {
+    }
+}
