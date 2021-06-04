63ed761af9c7f86dcb86eb922c52232209c0e8fd
Carlinhao <carsilva.ti@gmail.com>
2021-06-03 22:46:50 -0300

    Create UseCase

---
+++ src/employers.application/UseCases/Occupation/GetOccupationUseCaseAsync.cs
@@ -0,0 +1,22 @@
+﻿using employers.application.Interfaces.Occupation;
+using employers.domain.Interfaces.Repositories.Occupation;
+using employers.domain.Responses;
+using System.Threading.Tasks;
+
+namespace employers.application.UseCases.Occupation
+{
+    public class GetOccupationUseCaseAsync : IGetOccupationUseCaseAsync
+    {
+        private readonly IOccupationRepository _occupationRepository;
+
+        public GetOccupationUseCaseAsync(IOccupationRepository occupationRepository)
+        {
+            _occupationRepository = occupationRepository;
+        }
+
+        public async Task<ResultResponse> RunAsync()
+        {
+            return await _occupationRepository.GetAllAsync();
+        }
+    }
+}
