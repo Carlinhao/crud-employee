e9d4e129c057eeb21b5b119617633a75e34ee92e
Carlinhao <carsilva.ti@gmail.com>
2021-06-04 22:11:13 -0300

    Create use case to insert occupation

---
+++ src/employers.application/UseCases/Occupation/InsertOccupationUseCaseAsync.cs
@@ -0,0 +1,23 @@
+﻿using employers.application.Interfaces.Occupation;
+using employers.domain.Interfaces.Repositories.Occupation;
+using employers.domain.Requests;
+using employers.domain.Responses;
+using System.Threading.Tasks;
+
+namespace employers.application.UseCases.Occupation
+{
+    public class InsertOccupationUseCaseAsync : IInsertOccupationUseCaseAsync
+    {
+        private readonly IOccupationRepository _occupationRepository;
+
+        public InsertOccupationUseCaseAsync(IOccupationRepository occupationRepository)
+        {
+            _occupationRepository = occupationRepository;
+        }
+
+        public async Task<ResultResponse> RunAsync(OccupationRequest request)
+        {
+            return await _occupationRepository.InsertAsync(request);
+        }
+    }
+}
