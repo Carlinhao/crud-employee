d93a60ae9fcbba5035a2fb1eb39437765e4d3003
Carlinhao <carsilva.ti@gmail.com>
2021-06-04 22:49:41 -0300

    Add JsonPropertyName in request

--- src/employers.domain/Requests/OccupationRequest.cs
+++ src/employers.domain/Requests/OccupationRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace employers.domain.Requests
+﻿using System.Text.Json.Serialization;
+
+namespace employers.domain.Requests
 {
     public class OccupationRequest
     {
+        [JsonPropertyName("name_occupation")]
         public string NameOccupation { get; set; }
+
+        [JsonPropertyName("level_occupation")]
         public string LevelOccupation { get; set; }
     }
 }
--- src/employers.domain/Requests/OccupationUpdateRequest.cs
+++ src/employers.domain/Requests/OccupationUpdateRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace employers.domain.Requests
+﻿using System.Text.Json.Serialization;
+
+namespace employers.domain.Requests
 {
     public class OccupationUpdateRequest
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name_occupation")]
         public string NameOccupation { get; set; }
+
+        [JsonPropertyName("level_occupation")]
         public string LevelOccupation { get; set; }
     }
 }
