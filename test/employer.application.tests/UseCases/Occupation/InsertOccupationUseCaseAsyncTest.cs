0a107381cf32990479f46e765bbb6b6bdcc22ead
Carlinhao <carsilva.ti@gmail.com>
2021-06-09 23:35:44 -0300

    Configuring Mock and created test method

--- test/employer.application.tests/UseCases/Occupation/InsertOccupationUseCaseAsyncTest.cs
+++ test/employer.application.tests/UseCases/Occupation/InsertOccupationUseCaseAsyncTest.cs
@@ -1,7 +1,30 @@
-﻿namespace employer.application.tests.UseCases.Occupation
+﻿using employers.application.UseCases.Occupation;
+using employers.domain.Interfaces.Repositories.Occupation;
+using Moq;
+using Xunit;
+
+namespace employer.application.tests.UseCases.Occupation
 {
     public class InsertOccupationUseCaseAsyncTest
     {
-        
+        private readonly Mock<IOccupationRepository> _repository;
+
+        public InsertOccupationUseCaseAsyncTest()
+        {
+            _repository = new Mock<IOccupationRepository>();
+        }
+
+        [Fact(DisplayName = "Mudar")]
+        [Trait("Categoria", "Mudar")]
+        public void InsertOccupationUseCaseAsync_RunAsync_MustInsertAnOccupation()
+        {
+            // Arrange
+            
+
+            // Act
+
+            // Assert
+
+        }
     }
 }
