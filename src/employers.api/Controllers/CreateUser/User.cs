fe352a82ba9dcf67ba371f4b09cc22b19abfacfd
Carlinhao <carsilva.ti@gmail.com>
2021-06-13 23:39:34 -0300

    Create class to register an user.

---
+++ src/employers.api/Controllers/CreateUser/User.cs
@@ -0,0 +1,10 @@
+﻿namespace employers.api.Controllers.CreateUser
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+    }
+}
