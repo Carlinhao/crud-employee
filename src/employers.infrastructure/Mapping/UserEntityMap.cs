481eb3b24f94c510c3fc1c35db3d00e0a6887c5d
Carlinhao <carsilva.ti@gmail.com>
2021-12-19 23:30:05 -0300

    Mapping table and coluns to UserEntity

---
+++ src/employers.infrastructure/Mapping/UserEntityMap.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.FluentMap.Dommel.Mapping;
+using employers.domain.Entities.UserAuth;
+
+namespace employers.infrastructure.Mapping
+{
+    public class UserEntityMap : DommelEntityMap<UserEntity>
+    {
+        public UserEntityMap()
+        {
+            ToTable("User_Auth");
+
+            Map(x => x.Password).ToColumn("USER_PWD").IsKey();
+            Map(x => x.UserName).ToColumn("NAME_USER");
+        }
+    }
+}
