5658d4df8466c10488df10e7ff679d4c1d0d2ff9
Carlinhao <carsilva.ti@gmail.com>
2021-12-20 19:37:50 -0300

    Refactor method RefresUserInfo

--- src/employers.infrastructure/Repositories/UserAuth/UserAuthRepository.cs
+++ src/employers.infrastructure/Repositories/UserAuth/UserAuthRepository.cs
@@ -1,21 +1,22 @@
-﻿using Dapper;
-using employers.domain.Entities.UserAuth;
-using employers.domain.Interfaces.Repositories.UserAuth;
-using employers.domain.UserAuth;
-using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using employers.domain.Entities.UserAuth;
+using employers.domain.Interfaces.Repositories.UserAuth;
+using employers.domain.UserAuth;
+using Microsoft.Extensions.Configuration;
+using static Dapper.SqlMapper;
 
 namespace employers.infrastructure.Repositories.UserAuth
 {
     public class UserAuthRepository : IUserAuthRepository
     {
         private readonly IConfiguration _configuration;
-
+        private StringBuilder _stringBuilder;
         public IDbConnection Connection
         {
             get
@@ -27,6 +28,7 @@ namespace employers.infrastructure.Repositories.UserAuth
         public UserAuthRepository(IConfiguration configuration)
         {
             _configuration = configuration;
+            _stringBuilder = new StringBuilder();
         }
         
         public async Task<UserEntity> ValidateCredentials(UserInfoRequest userInfoRequest)
@@ -55,11 +57,26 @@ namespace employers.infrastructure.Repositories.UserAuth
             return result;
         }
 
-        public async Task<UserEntity> RefresUserInfo(UserEntity request)
+        public async Task RefresUserInfo(UserEntity request)
         {
-            var query = $" UPDATE Users WHERE Id = '{ request.Id }' " +
-                $"SET USR_NAM = '{ request.FullName }', PWD = '{ request.Password }', RFH_TOK = '{ request.AcessToken }'" +
-                $"RFH_TOK_EXP = '{ request.RefreshTokenExpire }' ";
+            var user = await GetIdByUserName(request);
+            
+            using IDbConnection conn = Connection;
+            conn.Open();
+
+            _stringBuilder.Append($"UPDATE User_Auth SET NAME_USER = '{request.UserName}', ");
+            _stringBuilder.Append($"USER_PWD = '{request.Password}', ");
+            _stringBuilder.Append($"ACESS_TOKEN = '{request.AcessToken}', ");
+            _stringBuilder.Append($"REFRESH_TOKEN = '{request.RefreshToken}', ");
+            _stringBuilder.Append($"REFRESH_TOKEN_EXPIRE = '{request.RefreshTokenExpire}' ");
+            _stringBuilder.Append($"WHERE ID_USER = {user.Id}");
+
+            await conn.QueryAsync<UserEntity>(_stringBuilder.ToString());
+        }
+
+        private async Task<UserEntity> GetIdByUserName(UserEntity request)
+        {
+            var query = $" SELECT * FROM User_Auth WHERE NAME_USER = '{ request.UserName }'";
 
             using IDbConnection conn = Connection;
             conn.Open();
