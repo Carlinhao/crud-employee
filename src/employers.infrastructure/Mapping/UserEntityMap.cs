3d78d6477baa3c9bd53d9851fae20993b1227c67
Carlinhao <carsilva.ti@gmail.com>
2021-12-20 19:40:47 -0300

    Add new coluns in to map UserEntityMap

--- src/employers.infrastructure/Mapping/UserEntityMap.cs
+++ src/employers.infrastructure/Mapping/UserEntityMap.cs
@@ -9,8 +9,14 @@ namespace employers.infrastructure.Mapping
         {
             ToTable("User_Auth");
 
-            Map(x => x.Password).ToColumn("USER_PWD").IsKey();
+            Map(x => x.Id).ToColumn("ID_USER").IsKey();
             Map(x => x.UserName).ToColumn("NAME_USER");
+            Map(x => x.FullName).ToColumn("FULL_NAME_USER");
+            Map(x => x.Password).ToColumn("USER_PWD");
+            Map(x => x.AcessToken).ToColumn("ACESS_TOKEN");
+            Map(x => x.RefreshToken).ToColumn("REFRESH_TOKEN");
+            Map(x => x.RefreshTokenExpire).ToColumn("REFRESH_TOKEN_EXPIRE");
+
         }
     }
 }
