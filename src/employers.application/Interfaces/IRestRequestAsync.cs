117832b2b1551ef2a5724c3058fc67d14abbcf78
Carlinhao <carsilva.ti@gmail.com>
2021-12-21 17:07:27 -0300

    Rename file .cs

---
+++ src/employers.application/Interfaces/IRestRequestAsync.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace employers.application.Interfaces
+{
+    public interface IRestRequestAsync<TResponse, TRequest>
+    {
+        Task<TResponse> RunAsync(TRequest request);
+    }
+}
