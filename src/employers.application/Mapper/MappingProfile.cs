f2709f8f92272dd9dd935395a68400add0c93277
Carlinhao <carsilva.ti@gmail.com>
2021-12-21 17:11:44 -0300

    mapping class CreateUserRequest to UserEntity

--- src/employers.application/Mapper/MappingProfile.cs
+++ src/employers.application/Mapper/MappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using employers.domain.Entities;
+using employers.domain.Entities.UserAuth;
+using employers.domain.Requests;
 using employers.domain.Responses;
 
 namespace employers.application.Mapper
@@ -9,6 +11,7 @@ namespace employers.application.Mapper
         public MappingProfile()
         {
             CreateMap<DepartmentEntity, DepartamentResponse>();
+            CreateMap<CreateUserRequest, UserEntity>();
         }
     }
 }
