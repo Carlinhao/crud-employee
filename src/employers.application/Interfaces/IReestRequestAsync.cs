1323aa47bf7186ac2cf5431565733eb747992450
Carlinhao <carsilva.ti@gmail.com>
2021-12-21 17:10:24 -0300

    Delete file .cs

--- src/employers.application/Interfaces/IReestRequestAsync.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace employers.application.Interfaces
-{
-    public interface IRestRequestAsync<TResponse, TRequest>
-    {
-        Task<TResponse> RunAsync(TRequest request);
-    }
-}
