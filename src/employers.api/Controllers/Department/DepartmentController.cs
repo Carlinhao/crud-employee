cbb8522c484cdc8c296ed998c2ae9355ec6a4724
Carlinhao <carsilva.ti@gmail.com>
2021-12-21 11:12:30 -0300

    Add response status code and documentation to swagger.

--- src/employers.api/Controllers/Department/DepartmentController.cs
+++ src/employers.api/Controllers/Department/DepartmentController.cs
@@ -1,14 +1,18 @@
-﻿using employers.application.Interfaces.Departament;
+﻿using System.Threading.Tasks;
+using employers.application.Interfaces.Departament;
 using employers.application.Interfaces.UseCases.Departament;
 using employers.application.Notifications;
 using employers.domain.Requests;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
 
 namespace employers.api.Controllers
 {
+    /// <summary>
+    /// API responsible for department management.
+    /// </summary>
     [Authorize]
     [ApiVersion("1")]
     [Route("api/v{version:apiVersion}/[controller]")]
@@ -24,7 +28,14 @@ namespace employers.api.Controllers
             _notificationMessages = notificationMessages;
         }
 
+        /// <summary>
+        /// Method responsible that return all Department.
+        /// </summary>
+        /// <param name="getAsync"></param>
+        /// <returns>Return a list of the department.</returns>
+        /// <response code="200">Return all departmen</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAll([FromServices] IGetDepartamentUseCaseAsync getAsync)
         {
             _logger.LogDebug("Buscando todos os Departamentos");
@@ -32,7 +43,17 @@ namespace employers.api.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Get a department by Id
+        /// </summary>
+        /// <param name="getAsync"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">Return a department by id.</response>
+        /// <response code="400">Return when return an error.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetById(
             [FromServices] IGetDepartamentByIdUseCaseAsync getAsync,
             int id)
@@ -47,7 +68,17 @@ namespace employers.api.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Insert a new department.
+        /// </summary>
+        /// <param name="postAsync"></param>
+        /// <param name="departmentRequest"></param>
+        /// <returns></returns>
+        /// <response code="200">Return number of department created</response>
+        /// <response code="400">Return when return an error message.</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PostAsync(
             [FromServices] IInsertDepartmentUseCaseAsync postAsync,
             [FromBody] DepartmentRequest departmentRequest)
