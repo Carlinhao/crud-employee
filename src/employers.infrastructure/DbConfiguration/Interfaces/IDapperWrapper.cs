4b1c2b031cce45e83fa21bccb6dea58b68493f7a
Carlinhao <carsilva.ti@gmail.com>
2022-05-28 00:19:52 -0300

    Removing attribute [ExcludeFromCodeCoverage].

--- src/employers.infrastructure/DbConfiguration/Interfaces/IDapperWrapper.cs
+++ src/employers.infrastructure/DbConfiguration/Interfaces/IDapperWrapper.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Data;
-using System.Diagnostics.CodeAnalysis;
 
 namespace employers.infrastructure.DbConfiguration.Interfaces
 {
-    [ExcludeFromCodeCoverage]
     public interface IDapperWrapper : IDisposable
     {
         IDbConnection GetConnection();
